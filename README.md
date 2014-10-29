606a0072aa6a6f34bf83955b9595ddfe4b998e9f
Damien Matabon <damien.matabon@gmail.com>
2014-10-29 02:28:26 -0400

    Add MongoDB support

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ Offers a sortable feature for your Symfony2 admin listing
 Implementation for the Sonata Admin bundle explained in the cookbook
 
 https://github.com/sonata-project/SonataAdminBundle/blob/master/Resources/doc/cookbook/recipe_sortable_listing.rst
+
+### Configuration
+
+By default, this extension works with Doctrine ORM, but you can choose to use Doctrine MongoDB by defining the driver configuration : 
+
+``` yaml
+# app/config/config.yml
+pix_sortable_behavior:
+    db_driver: mongodb # default value : orm
+```
--- Services/PositionHandler.php
+++ Services/PositionHandler.php
@@ -10,21 +10,8 @@
 
 namespace Pix\SortableBehaviorBundle\Services;
 
-use Doctrine\ORM\EntityManager;
-
-class PositionHandler
+abstract class PositionHandler
 {
-
-    /**
-     *
-     * @var EntityManager
-     */
-    protected $em;
-
-    public function __construct(EntityManager $entityManager)
-    {
-        $this->em = $entityManager;
-    }
 
 
     public function getPosition($object, $position, $last_position)
@@ -61,18 +48,6 @@ class PositionHandler
 
     }
 
-    public function getLastPosition($entity)
-    {
-
-        $query = $this->em->createQuery('SELECT MAX(m.position) FROM '.$entity.' m');
-        $result = $query->getResult();
-
-        if (array_key_exists(0, $result)) {
-            return intval($result[0][1]);
-        }
-
-        return 0;
-    }
-
+    abstract public function getLastPosition($entity);
 
 }
