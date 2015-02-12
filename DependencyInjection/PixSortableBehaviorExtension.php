d9ce1deee783f88f5c7575ed84278a5890da962e
Volker von Hoesslin <volker.von.hoesslin@empora.com>
2015-02-12 13:21:37 +0100

    fix uses

--- DependencyInjection/PixSortableBehaviorExtension.php
+++ DependencyInjection/PixSortableBehaviorExtension.php
@@ -14,6 +14,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Alias;
 
 /**
  * This is the class that loads and manages your bundle configuration
