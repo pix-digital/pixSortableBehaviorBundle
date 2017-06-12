374d90f8f94d194805b66b9cd675ff4282080b62
Adrien Foulon <adrien.foulon.lfgeb@gmail.com>
2017-06-12 23:00:52 +0200

    NEW_POSITION sended relatively to previous pos

--- Resources/public/js/init.js
+++ Resources/public/js/init.js
@@ -17,6 +17,7 @@ var DraggableTable = function () {
 };
 
 DraggableTable.prototype.init = function (node, settings) {
+    var prevPosition = 0;
     $(node).sortable({
         'handle': '.js-sortable-move',
         'axis': 'y',
@@ -33,13 +34,16 @@ DraggableTable.prototype.init = function (node, settings) {
             });
             return ui;
         },
+        'start': function(event, ui) {
+            prevPosition = ui.item.index();
+        },
         'update': function(event, ui) {
             var moved = $(ui.item).find('.js-sortable-move');
             var newPosition = ui.item.index();
 
             $.ajax({
                 'type': 'GET',
-                'url': moved.data('url').replace('NEW_POSITION', newPosition),
+                'url': moved.data('url').replace('NEW_POSITION', newPosition - prevPosition),
                 'dataType': 'json',
                 'success': function(data) {
                     $(document).trigger("pixSortableBehaviorBundle.success", [data]);
